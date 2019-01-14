565c2ab5d12eede621f3426bb2c7bf445bfe5d1c
Antonio Miguel Pavon <Antonio Miguel Pavon@DESKTOP-9BOG7HL>
2019-01-14 13:09:27 +0100

    Primera fase del proyecto SaltyCats

--- src/saltycat/SaltyCat.java
+++ src/saltycat/SaltyCat.java
@@ -1,39 +1,43 @@
 package saltycat;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+ import javafx.application.Application;
+ import javafx.geometry.Rectangle2D;
+ import javafx.scene.Group;
+ import javafx.scene.Scene; 
+ import javafx.scene.image.Image;
+ import javafx.scene.image.ImageView;
+ import javafx.scene.layout.HBox;
+ import javafx.scene.paint.Color;
+ import javafx.stage.Stage; 
 
 public class SaltyCat extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public void start(Stage stage){
+  
+    Image image = new Image("fondoBueno.png");
+    ImageView fondoJuego = new ImageView();
+    fondoJuego.setImage(image);
+//    fondoJuego.setScaleX(2);
+//    fondoJuego.setScaleY(2);
+ 
     
+    Group root = new Group();
+    Scene scene = new Scene(root);
+    scene.setFill(Color.BLACK);
+    HBox prueba = new HBox();
+    prueba.getChildren().add(fondoJuego);
+    root.getChildren().add(prueba);
+    
+    stage.setTitle("Salty Cats");
+    stage.setWidth(1200);
+    stage.setHeight(650);
+    stage.setScene(scene); 
+    stage.sizeToScene(); 
+    stage.show();
+  }
+  
+  public static void main(String[] args) {
+         Application.launch(args);
+     }
 }
