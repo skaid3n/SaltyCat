9026638bd3ab53f30f307001c226d6dcbd9ac66a
Antonio Miguel Pavon <Antonio Miguel Pavon@DESKTOP-9BOG7HL>
2019-01-28 13:00:29 +0100

    7 fase del proyecto SaltyCats (crear los metodos )

--- src/saltycat/SaltyCat.java
+++ src/saltycat/SaltyCat.java
@@ -16,7 +16,7 @@ import javafx.scene.text.Text;
  import javafx.stage.Stage; 
 
 public class SaltyCat extends Application {
-
+    
 int posicionX= 1024;
 int posicionY= 768;
 
@@ -26,12 +26,28 @@ int pos2Fondo1= 1024;
 int pos1FondoCamino= 0;
 int pos2FondoCamino= 1024;
 
-
 int velocidad= -1 ;
 int velocidadCamino = -2;
-
+           
 int score;
 Text textScore;
+
+int velocidadG=-2;
+
+ImageView fondoJuego = new ImageView();
+ImageView fondoJuego1 = new ImageView();
+
+ImageView fondoCamino = new ImageView();
+ImageView fondoCamino1 = new ImageView();
+
+ImageView gatoMuerto = new ImageView();
+ImageView gatoSaltar = new ImageView();
+ImageView gatoCaminar = new ImageView();
+
+ImageView gallinaG1 = new ImageView();
+ImageView gallinaG2 = new ImageView();
+ImageView gallinaG3 = new ImageView();
+ImageView gallinaG4 = new ImageView();
 
 //Gallina 1
 int pos1Gallina1= 1100;
@@ -41,120 +57,220 @@ int pos1Gallina2= 1400;
 int pos1Gallina3= 1700;
 //Gallina 4
 int pos1Gallina4= 2000;
+            
 
-int velocidadG=-2;
-
+Group root = new Group();
 Group gatoMovimiento;
 Group gatoSalto;
+Group gatoDie;
 
     @Override
   public void start(Stage stage){
-
+     
+      fondosYGatos();
     
     //Texto de score
     textScore = new Text("0");
-      
-    Group root = new Group();
+
     Scene scene = new Scene(root, posicionX, posicionY);
     stage.setTitle("Salty Cats");
     stage.setScene(scene);
     stage.show();
     
-    Random aleatorio = new Random();
+    
+    AnimationTimer animationFondo = new AnimationTimer(){
+    @Override
+    
+        public void handle (long now){
+                fondoMontañas();
+                fondoCamino();
+            
+        } 
+    };
+    
+    animationFondo.start();
+//  
+//            scene.setOnKeyPressed((KeyEvent event)-> {
+//            switch(event.getCode()){
+//                case W:
+//                    gatoMovimiento.setVisible(false);
+////                    gatoSalto.setVisible(true);
+//                    gatoSalto.setLayoutX(300);
+//                    
+//                    break;
+//            }
+//        });
+//        scene.setOnKeyReleased((KeyEvent event) -> {
+//             
+//        }); 
   
-      // Imagen fondo montañas 
-    Image imageFondo = new Image("fondo.png");
-    
-    ImageView fondoJuego = new ImageView();
-    ImageView fondoJuego1 = new ImageView();
-    
-    fondoJuego.setImage(imageFondo);
-    fondoJuego1.setImage(imageFondo);
-    
-    fondoJuego.setFitWidth(1024);
-    fondoJuego.setFitHeight(768);
-    fondoJuego1.setFitWidth(1024);
-    fondoJuego1.setFitHeight(768);
-    
-    root.getChildren().add(fondoJuego);
-    root.getChildren().add(fondoJuego1);
-    
+  }
+       private void fondosYGatos(){
+                   // Imagen fondo montañas 
+        Image imageFondo = new Image("fondo.png");
+
+        fondoJuego = new ImageView();
+        fondoJuego1 = new ImageView();
+
+        fondoJuego.setImage(imageFondo);
+        fondoJuego1.setImage(imageFondo);
+
+        fondoJuego.setFitWidth(1024);
+        fondoJuego.setFitHeight(768);
+        fondoJuego1.setFitWidth(1024);
+        fondoJuego1.setFitHeight(768);
+
+        root.getChildren().add(fondoJuego);
+        root.getChildren().add(fondoJuego1);
         
-       // Fondo Camino 
-    Image imageFondoCamino = new Image("fondoCamino.png");
+        Image imageFondoCamino = new Image("fondoCamino.png");
+
+        fondoCamino = new ImageView();
+        fondoCamino1 = new ImageView();
+
+        fondoCamino.setImage(imageFondoCamino);
+        fondoCamino1.setImage(imageFondoCamino);
+
+        fondoCamino.setFitWidth(1024);
+        fondoCamino.setFitHeight(768);
+
+        fondoCamino1.setFitWidth(1024);
+        fondoCamino1.setFitHeight(768);
+
+        root.getChildren().add(fondoCamino);
+        root.getChildren().add(fondoCamino1);
+        
+                    
+        Image imageMuerte = new Image ("gatoMuerte.gif");
+        gatoMuerto = new ImageView();
+        gatoMuerto.setImage(imageMuerte);
+        gatoMuerto.setScaleX(4);
+        gatoMuerto.setScaleY(4);
+            
+        Image imageSalto = new Image ("saltoGato.gif");
+        gatoSaltar = new ImageView();
+        gatoSaltar.setImage(imageSalto);
+        gatoSaltar.setScaleX(4);
+        gatoSaltar.setScaleY(4);
+      
+                   
+        Image imageCaminar = new Image ("catWalk.gif");
+        gatoCaminar = new ImageView();
+        gatoCaminar.setImage(imageCaminar);
+        gatoCaminar.setScaleX(4);
+        gatoCaminar.setScaleY(4);
+ 
+       }
+       
+       private void gashinitas(){
+        Image imageChicken = new Image ("chicken.gif");
     
-    ImageView fondoCamino = new ImageView();
-    ImageView fondoCamino1 = new ImageView();
-    
-    fondoCamino.setImage(imageFondoCamino);
-    fondoCamino1.setImage(imageFondoCamino);
-    
-    fondoCamino.setFitWidth(1024);
-    fondoCamino.setFitHeight(768);
-    
-    fondoCamino1.setFitWidth(1024);
-    fondoCamino1.setFitHeight(768);
-    
-    root.getChildren().add(fondoCamino);
-    root.getChildren().add(fondoCamino1);
-    
-    // Gato Saltando
-    Image imageSalto = new Image ("saltoGato.gif");
-    ImageView gatoSaltar = new ImageView();
-    gatoSaltar.setImage(imageSalto);
-    gatoSaltar.setScaleX(4);
-    gatoSaltar.setScaleY(4);
-    
-    // Gallinas ANDANDO
-    Image imageChicken = new Image ("chicken.gif");
-    
-    ImageView gallinaG1 = new ImageView();
-    ImageView gallinaG2 = new ImageView();
-    ImageView gallinaG3 = new ImageView();
-    ImageView gallinaG4 = new ImageView();
-    
-    gallinaG1.setImage(imageChicken);
-    gallinaG2.setImage(imageChicken);
-    gallinaG3.setImage(imageChicken);
-    gallinaG4.setImage(imageChicken);
-    
-    gallinaG1.setScaleX(2);
-    gallinaG1.setScaleY(2);
-    
-    gallinaG2.setScaleX(2);
-    gallinaG2.setScaleY(2);
-    
-    gallinaG3.setScaleX(2);
-    gallinaG3.setScaleY(2);
-    
-    gallinaG4.setScaleX(2);
-    gallinaG4.setScaleY(2);
-    
-    
-    Group gallinaGrupo1 = new Group();
-    gallinaGrupo1.getChildren().add(gallinaG1);
-    gallinaGrupo1.setLayoutX(pos1Gallina1);
-    gallinaGrupo1.setLayoutY(600);
-    root.getChildren().add(gallinaGrupo1);
-    
-    Group gallinaGrupo2 = new Group();
-    gallinaGrupo2.getChildren().add(gallinaG2);
-    gallinaGrupo2.setLayoutX(pos1Gallina2);
-    gallinaGrupo2.setLayoutY(600);
-    root.getChildren().add(gallinaGrupo2);
-    
-    Group gallinaGrupo3 = new Group();
-    gallinaGrupo3.getChildren().add(gallinaG3);
-    gallinaGrupo3.setLayoutX(pos1Gallina3);
-    gallinaGrupo3.setLayoutY(600);
-    root.getChildren().add(gallinaGrupo3);
-    
-    Group gallinaGrupo4 = new Group();
-    gallinaGrupo4.getChildren().add(gallinaG4);
-    gallinaGrupo4.setLayoutX(pos1Gallina4);
-    gallinaGrupo4.setLayoutY(600);
-    root.getChildren().add(gallinaGrupo4);
-    
+        gallinaG1 = new ImageView();
+        gallinaG2 = new ImageView();
+        gallinaG3 = new ImageView();
+        gallinaG4 = new ImageView();
+
+        gallinaG1.setImage(imageChicken);
+        gallinaG2.setImage(imageChicken);
+        gallinaG3.setImage(imageChicken);
+        gallinaG4.setImage(imageChicken);
+
+        gallinaG1.setScaleX(2);
+        gallinaG1.setScaleY(2);
+
+        gallinaG2.setScaleX(2);
+        gallinaG2.setScaleY(2);
+
+        gallinaG3.setScaleX(2);
+        gallinaG3.setScaleY(2);
+
+        gallinaG4.setScaleX(2);
+        gallinaG4.setScaleY(2);
+       }
+       
+       private void fondoMontañas(){
+        fondoJuego.setX(pos1Fondo);
+        fondoJuego1.setX(pos2Fondo1);
+        pos1Fondo+=velocidad;
+        pos2Fondo1+=velocidad;
+        
+        if (pos2Fondo1 == 0){
+            pos1Fondo = 1024;
+        }
+        if (pos1Fondo == 0){
+            pos2Fondo1 = 1024;         
+        }
+       }
+      
+       private void fondoCamino(){
+           
+        fondoCamino.setX(pos1FondoCamino);
+        fondoCamino1.setX(pos2FondoCamino);
+        pos1FondoCamino+= velocidadCamino;
+        pos2FondoCamino+= velocidadCamino;
+        
+        if (pos2FondoCamino==0){
+            pos1FondoCamino = 1024;
+        }
+        if (pos1FondoCamino== 0){
+            pos2FondoCamino = 1024;
+        }
+       }
+  
+       private void crearGatoMuerto(){
+            gatoDie = new Group();
+            gatoDie.getChildren().add(gatoMuerto);
+            gatoDie.setLayoutX(150);
+            gatoDie.setLayoutY(600);
+            root.getChildren().add(gatoDie); 
+       }
+       
+       private void crearSaltoGato(){
+
+            gatoSalto = new Group();
+            gatoSalto.getChildren().add(gatoSaltar);
+            gatoSalto.setLayoutX(150);
+            gatoSalto.setLayoutY(600);
+            root.getChildren().add(gatoSalto);
+       }
+       
+       private void crearMovimientoGato(){
+
+            gatoMovimiento = new Group();
+            gatoMovimiento.getChildren().add(gatoCaminar);
+            gatoMovimiento.setLayoutX(150); 
+            gatoMovimiento.setLayoutY(600);
+            root.getChildren().add(gatoMovimiento);
+     }
+       
+       private void gallinasGrupo(){
+
+        Random aleatorio = new Random();
+  
+        Group gallinaGrupo1 = new Group();
+        gallinaGrupo1.getChildren().add(gallinaG1);
+        gallinaGrupo1.setLayoutX(pos1Gallina1);
+        gallinaGrupo1.setLayoutY(600);
+        root.getChildren().add(gallinaGrupo1);
+
+        Group gallinaGrupo2 = new Group();
+        gallinaGrupo2.getChildren().add(gallinaG2);
+        gallinaGrupo2.setLayoutX(pos1Gallina2);
+        gallinaGrupo2.setLayoutY(600);
+        root.getChildren().add(gallinaGrupo2);
+
+        Group gallinaGrupo3 = new Group();
+        gallinaGrupo3.getChildren().add(gallinaG3);
+        gallinaGrupo3.setLayoutX(pos1Gallina3);
+        gallinaGrupo3.setLayoutY(600);
+        root.getChildren().add(gallinaGrupo3);
+
+        Group gallinaGrupo4 = new Group();
+        gallinaGrupo4.getChildren().add(gallinaG4);
+        gallinaGrupo4.setLayoutX(pos1Gallina4);
+        gallinaGrupo4.setLayoutY(600);
+        root.getChildren().add(gallinaGrupo4);
+
     
  
         int separacion = aleatorio.nextInt(300);
@@ -167,48 +283,13 @@ Group gatoSalto;
         pos1Gallina3= pos1Gallina2 + separacion;
    
         separacion = aleatorio.nextInt(300);
-        pos1Gallina4= pos1Gallina3 + separacion;
-
-    
-    
-    AnimationTimer animationFondo = new AnimationTimer(){
-    @Override
-    
-    
-    
-    
-    //Movimiento de las montañas y el camino
-    
-    public void handle (long now){
-        fondoJuego.setX(pos1Fondo);
-        fondoJuego1.setX(pos2Fondo1);
-        pos1Fondo+=velocidad;
-        pos2Fondo1+=velocidad;
-        
-        if (pos2Fondo1 == 0){
-            pos1Fondo = 1024;
-        }
-        if (pos1Fondo == 0){
-            pos2Fondo1 = 1024;         
-        }
-        
-        fondoCamino.setX(pos1FondoCamino);
-        fondoCamino1.setX(pos2FondoCamino);
-        pos1FondoCamino+= velocidadCamino;
-        pos2FondoCamino+= velocidadCamino;
-        
-        if (pos2FondoCamino==0){
-            pos1FondoCamino = 1024;
-        }
-        if (pos1FondoCamino== 0){
-            pos2FondoCamino = 1024;
-        }
+        pos1Gallina4= pos1Gallina3 + separacion;      
         
         gallinaGrupo1.setLayoutX(pos1Gallina1);
         pos1Gallina1+=velocidadG;
         
         if (pos1Gallina1 == -110){
-            pos1Gallina1 = pos1Gallina4+300 ;
+            pos1Gallina1 = 1100 ;
         }      
         
         gallinaGrupo2.setLayoutX(pos1Gallina2);
@@ -230,67 +311,8 @@ Group gatoSalto;
         
         if (pos1Gallina4 == -110){
             pos1Gallina4 = pos1Gallina3+300;
-        } 
-     
-        
-    } 
-    };
-    animationFondo.start();
-  
-            scene.setOnKeyPressed((KeyEvent event)-> {
-            switch(event.getCode()){
-                case W:
-                    gatoMovimiento.setVisible(false);
-//                    gatoSalto.setVisible(true);
-                    gatoSalto.setLayoutX(300);
-                    
-                    break;
-            }
-        });
-        scene.setOnKeyReleased((KeyEvent event) -> {
-             
-        }); 
-     
-     // Gato Caminando
-    Image imageCaminar = new Image ("catWalk.gif");
-    ImageView gatoCaminar = new ImageView();
-    gatoCaminar.setImage(imageCaminar);
-    gatoCaminar.setScaleX(4);
-    gatoCaminar.setScaleY(4);
- 
-    
-    // Gato Muerto
-    Image imageMuerte = new Image ("gatoMuerte.gif");
-    ImageView gatoMuerto = new ImageView();
-    gatoMuerto.setImage(imageMuerte);
-    gatoMuerto.setScaleX(4);
-    gatoMuerto.setScaleY(4);
-    
-    
-     // Creacion de grupos
-
-    // Gato movimiento andar
-    gatoMovimiento = new Group();
-    gatoMovimiento.getChildren().add(gatoCaminar);
-    gatoMovimiento.setLayoutX(150);
-    gatoMovimiento.setLayoutY(600);
-    root.getChildren().add(gatoMovimiento);
-    
-    // Gato movimiento saltar
-    gatoSalto = new Group();
-    gatoSalto.getChildren().add(gatoSaltar);
-    gatoSalto.setLayoutX(150);
-    gatoSalto.setLayoutY(600);
-    root.getChildren().add(gatoSalto);
-    
-    // Gato muerto
-//    Group gatoDie = new Group();
-//    gatoDie.getChildren().add(gatoMuerto);
-//    gatoDie.setLayoutX(150);
-//    gatoDie.setLayoutY(600);
-//    root.getChildren().add(gatoDie);
-    
-  }
+        }         
+       }
   
   private void resetGame(){
       score = 0;
